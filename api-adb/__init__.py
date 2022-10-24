ec10e6b3dc8761f1a2b72fd1ca958117b0ceafdc
Roberto Paes <robertocpaes@gmail.com>
2022-10-24 18:29:52 -0300

    adicionando opção para usar um número de celular específico

---
+++ api-adb/__init__.py
@@ -0,0 +1,2 @@
+__app_name__ = "api-adb"
+__version__ = "0.0.1"
--- api-adb/__main__.py
+++ api-adb/__main__.py
@@ -1,7 +1,8 @@
 
 # adb emu sms send 5551234 Hello Android
 import json
-import os
+import winreg as reg
+import os       
 from flask import Flask, request
 app = Flask(__name__)
  
@@ -13,7 +14,10 @@ def hello_world():
 def sms_sender():
     request_data = request.get_json()
     message = request_data['message']
-    command =  'adb emu sms send 5551234 ' + message
+    numero = '5551234'
+    if 'numero' in request_data:
+        numero = request_data['numero']
+    command =  f'adb emu sms send {numero} {message}'
     os.system(command)
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
